d85ca8e6911dcaf8e53fb220fbcdee5cd4fc1b9f
Dmitri_Sidorov <kin19861@rambler.ru>
2025-10-25 00:30:34 +0300

    fixit

--- config/settings.py
+++ config/settings.py
@@ -172,7 +172,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Настройки для Celery
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0") # Например, Redis, который по умолчанию работает на порту 6379
+CELERY_BROKER_URL = os.getenv(
+    "CELERY_BROKER_URL", "redis://localhost:6379/0"
+)  # Например, Redis, который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", CELERY_BROKER_URL)
